a8c18b64d8765e6b5d686737c4792de48547b15e
Delphine Pothin <88280654+delpot@users.noreply.github.com>
2023-03-07 10:06:23 +0100

    refactor: Rename variable

--- Statistics.py
+++ Statistics.py
@@ -2,4 +2,4 @@ class Statistics:
     def __init__(self, health_points: int, attacks: int, experiment: int):
         self.health_points = health_points
         self.attacks = attacks
-        self.experiment = experiment
+        self.experience = experience
