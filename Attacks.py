10de2550980a32a9fac691988178086c8e0df6cc
Delphine Pothin <88280654+delpot@users.noreply.github.com>
2023-03-07 10:07:12 +0100

    feat: Add Attacks class

---
+++ Attacks.py
@@ -0,0 +1,3 @@
+class Attacks:
+    def __init__(self, basic_attack: int): # more attacks to come
+        self.basic_attack = basic_attack
--- Statistics.py
+++ Statistics.py
@@ -1,5 +1,6 @@
+import Attacks
 class Statistics:
-    def __init__(self, health_points: int, attacks: int, experiment: int):
+    def __init__(self, health_points: int, attacks: Attacks, experience: int):
         self.health_points = health_points
         self.attacks = attacks
         self.experience = experience
