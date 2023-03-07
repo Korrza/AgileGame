3ce60c3b0375f182339d5eae664a05781477aa9c
Delphine Pothin <88280654+delpot@users.noreply.github.com>
2023-03-07 10:53:53 +0100

    feat: Add earn_xp method

--- Statistics.py
+++ Statistics.py
@@ -1,7 +1,11 @@
 from Actions import Attacks, Healing
 class Statistics:
-    def __init__(self, max_hp: int, current_hp: int, attacks: Attacks, experience: int):
+    def __init__(self, max_hp: int, current_hp: int, attacks: Attacks, xp: int):
         self.max_hp = max_hp
         self.current_hp = current_hp
         self.attacks = attacks
-        self.experience = experience
+        self.xp = xp
+
+    def earn_xp(self, xp_input):
+        self.xp += xp_input
+        return self.xp
